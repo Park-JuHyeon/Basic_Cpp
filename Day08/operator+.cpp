adc447e12aada7ca9e54c26a88cd4f36e9da6dfb
Park-JuHyeon <j-hyeon97@naver.com>
2023-04-04 09:20:17 +0900

    Update operator+.cpp

--- Day08/operator+.cpp
+++ Day08/operator+.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
 using namespace std;
 
+// Myclass 클래스 정의
 class Myclass
 {
 private:
-	int num1,num2;
-	const char* name;
+	int num1, num2;  // 멤버 변수 선언
+	const char* name; // const char* 형태의 멤버 변수 선언
 public:
+	// 생성자 정의 (num1, num2, name 멤버 변수 초기화)
 	Myclass(int n1 = 0, int n2 = 0, const char* aname = nullptr) : num1(n1), num2(n2), name(aname)
 	{}
+	// 멤버 변수 값을 출력하는 함수
 	void showData()
 	{
 		cout << num1 << ", " << num2 << ", " << name << endl;
 	}
-
 };
 
 int main(void)
 {
-	Myclass std(10, 20, "Park");
-	std.showData();
-	Myclass std1(std);		// std1 = std
-	std1.showData();
+	Myclass std(10, 20, "Park");  // 객체 생성, 생성자 호출
+	std.showData();               // 멤버 변수 값 출력
+	Myclass std1(std);            // 객체 복사, 복사 생성자 호출
+	std1.showData();              // 복사된 멤버 변수 값 출력
 
-	Myclass std2;
-	std2 = std1;
-	std2.showData();
+	Myclass std2;                 // 객체 생성, 기본 생성자 호출
+	std2 = std1;                  // 객체 대입, 대입 연산자 호출
+	std2.showData();              // 복사된 멤버 변수 값 출력
 
 	return 0;
 }
